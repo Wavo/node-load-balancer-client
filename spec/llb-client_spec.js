b83865a111a0254437f21361526689ff5027d5c9
Diogo Biazus <diogob@gmail.com>
2012-02-13 08:57:55 -0200

    finished register method basic implementation

--- spec/llb-client_spec.js
+++ spec/llb-client_spec.js
@@ -11,15 +11,31 @@ var client = new LLBClient(config);
 
 describe("LLBClient", function(){
   describe("#register", function(){
+    var req = {write: function(){}, end: function(){}};
+
     beforeEach(function(){
-      spyOn(client.http, "request");
+      spyOn(req, "write");
+      spyOn(req, "end");
+      spyOn(client.http, "request").andReturn(req);
+      client.register();
+    });
+
+    it("should write instace data to request body", function(){
+      expect(req.write).toHaveBeenCalledWith(JSON.stringify({
+        class: 'ClassA',
+        version: '0.0.1',
+        host: '127.0.0.1',
+        port: 9000,
+      }));
+      expect(req.end).toHaveBeenCalled();
     });
 
     it("should make an http request to the server and post the register command in the body", function(){
-      client.register();
       expect(client.http.request).toHaveBeenCalledWith({
         host: config.serverHost,
-        port: config.serverPort
+        port: config.serverPort,
+        method: 'POST',
+        path: '/router/instances'
       });
     });
 
